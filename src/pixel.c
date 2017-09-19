6f8ae25d60b34a16c1d9d522d98ff9878aed58b4
brandon-gong <brandon.gong@brandongong.org>
2017-09-19 18:58:19 -0500

    cairo works

--- src/pixel.c
+++ src/pixel.c
@@ -1,4 +1,6 @@
 #include <gtk/gtk.h>
+#include <cairo.h>
+#include <math.h>
 
 #define WIDTH 900
 #define HEIGHT 600
@@ -11,6 +13,23 @@ static gboolean delete_event( GtkWidget *widget,
 
 static void destroy(GtkWidget *widget, gpointer data) {
     gtk_main_quit();
+}
+
+static void on_draw(GtkWidget *widget, cairo_t *cr, gpointer data) {
+    GtkWidget *win = gtk_widget_get_toplevel(widget);
+
+      int width, height;
+      gtk_window_get_size(GTK_WINDOW(win), &width, &height);
+
+      cairo_set_line_width(cr, 9);
+      cairo_set_source_rgb(cr, 0.69, 0.19, 0);
+
+      cairo_translate(cr, width/2, height/2);
+      cairo_arc(cr, 0, 0, 50, 0, 2 * M_PI);
+      cairo_stroke_preserve(cr);
+
+      cairo_set_source_rgb(cr, 0.3, 0.4, 0.6);
+      cairo_fill(cr);
 }
 
 int main(int argc, char *argv[]) {
@@ -35,7 +54,15 @@ int main(int argc, char *argv[]) {
     //                 (r.height / 2 - HEIGHT / 2));
     gtk_window_set_title(GTK_WINDOW (window), "Pixel v0.1.0");
     gtk_window_set_icon_from_file(GTK_WINDOW (window), "../res/icon.png", NULL);
-    gtk_widget_show (window);
+
+    GtkWidget *drawing_area;
+    drawing_area = gtk_drawing_area_new();
+    g_signal_connect (G_OBJECT (drawing_area), "draw", G_CALLBACK (on_draw),
+    NULL);
+
+    gtk_container_add(GTK_CONTAINER(window), drawing_area);
+    gtk_widget_show(drawing_area);
+    gtk_widget_show(window);
 
     gtk_main ();
     return 0;
